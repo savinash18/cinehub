29d580e7ad8fd4a8e4398c42a662041e1717c4af
Sai Avinash <avinash.palaparthi24@sasi.ac.in>
2026-09-08 22:27:06 +0530

    Fix Gmail SMTP connection

--- server.js
+++ server.js
@@ -44,11 +44,13 @@ app.get("/register.html/", (req, res) => {
 // ==================================================
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
 
 
