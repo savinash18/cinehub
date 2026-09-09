ea35a540741a08b2a17954083de0af30dc20423c
savinash18 <avinash.palaparthi24@sasi.ac.in>
2026-09-09 14:05:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1037,7 +1037,7 @@ app.post(
                try {
     const { data, error } = await resend.emails.send({
         from: "CineHub <onboarding@resend.dev>",
-        to: "delivered@resend.dev",
+        to: "avinash.palaparthi24@sasi.ac.in",
         subject: "CineHub Password Reset Code",
         text:
             `Your CineHub password reset verification code is: ${otp}\n\n` +
